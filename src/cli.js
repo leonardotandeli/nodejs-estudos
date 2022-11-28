d66e95e47dbee9a612f8b8712cba92ed3ffc84bb
Leonardo <leonardotandelicw@outlook.com>
2022-11-28 09:11:31 -0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,15 @@
 import pegaArquivo from "./index.js";
+import chalk from 'chalk';
+
 
 const caminho = process.argv;
 
 
-pegaArquivo(caminho[2])
+async function processaTexto(caminho) {
+        const resultado = await pegaArquivo(caminho[2])
+         console.log(chalk.yellow("lista de links"), resultado)
+
+
+}
+
+processaTexto(caminho)
