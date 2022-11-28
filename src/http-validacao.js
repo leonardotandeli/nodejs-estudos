b42c4bf5bf6ffff5ecd11af96e44819b58d06444
Leonardo <leonardotandelicw@outlook.com>
2022-11-28 10:31:26 -0300

    Update http-validacao.js

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,4 +1,47 @@
+import chalk from 'chalk';
 
-export default function listaValidada(listaDeLinks) {
-return "entrou na funcao"
+function extraiLinks(arrLinks) {
+    return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
 }
+
+async function checaStatus(listaURLs) {
+    const arrStatus = await Promise.all(
+
+        listaURLs.map(async (url) => {
+            try {
+                const response = await fetch(url)
+                return response.status
+            } catch(erro) {
+                return manejaErros(erro)
+            }
+        })
+
+
+    )
+return arrStatus
+}
+
+function manejaErros(erro) {
+ if (erro.cause.code === "ENOTFOUND") {
+    return "link não encontrado."
+ } else {
+    return "ocorreu algum erro"
+ }
+}
+
+
+export default async function listaValidada(listaDeLinks) {
+const links =  extraiLinks(listaDeLinks)
+const status = await checaStatus(links)
+
+
+return listaDeLinks.map((objeto, indice) => 
+({ ...objeto,
+    status: status[indice]
+
+})
+)
+
+}
+
+
